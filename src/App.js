e7cd055c462d26953e74442ec794c42c1f44cafb
Ryan Eldon <47878230+rdogstyle@users.noreply.github.com>
2021-04-22 15:44:40 -0400

    test homepage

--- src/App.js
+++ src/App.js
@@ -2,24 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="app__header"></header>
+			<body className="app__body">
+				<h1>SUP!</h1>
+			</body>
+		</div>
+	);
 }
 
 export default App;
